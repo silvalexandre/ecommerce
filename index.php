9000decdcb13b62acd16348ee5084819a5502f83
Alexandre Silva <alexandresoussilva@gmail.com>
2022-02-28 14:05:58 -0300

    atualização do esquema de segurança

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ $app->get('/admin/login', function(){
 });
 
 $app->post('/admin/login', function(){
+	
 	User::login($_POST["login"], $_POST["password"]);
 
 	header("Location: /admin");
@@ -151,6 +152,83 @@ $app->post('/admin/users/:iduser', function($iduser){
 
 	header("Location: /admin/users");
 	exit;
+
+});
+
+$app->get('/admin/forgot', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("forgot");
+});
+
+
+$app->post('/admin/forgot', function(){
+	$user = User::getForgot($_POST["email"]);
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get('/admin/forgot/sent', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get('/admin/forgot/reset', function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl('forgot-reset', array(
+
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+
+	));
+
+
+});
+
+$app->post('/admin/forgot/reset', function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setForgotUsed($forgot['idrecovery']);
+
+	$user = new User();
+
+	$user->get((int)$forgot['iduser']);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+
+		"cost"=>12
+
+	]);
+
+	$user->setPassword($password);
+
+
+	$page = new PageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl('forgot-reset-success');
+
 
 });
 
